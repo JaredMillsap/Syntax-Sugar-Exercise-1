9a6bada96d84b7877e866d8850349c8c4ca24c4e
JaredMillsap <jmillsap23@gmail.com>
2021-10-05 16:51:32 -0500

    Syntax Sugar Exercise 1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Syntax_Sygar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            int answer = 4;
+            string response;
+
+            response = (answer < 9) ? "is less than 9" : "greather than or equal to nine";
+
+            Console.WriteLine($"{response}");
+        }
+
+
+
+    }
+
+
+
+
+
+    
+}
+
+
+
